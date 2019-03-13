766e7691326a51654e9999f0f288bf0ab2d398ef
Alexa Tang <alexatang@cpp.edu>
2019-03-12 18:39:08 -0700

    2

--- Midterm2560/Test/test.c
+++ Midterm2560/Test/test.c
@@ -8,7 +8,26 @@
 
 int main()
 {
-	printf("hello");
+	int rows_Columns;
+	int counter = 1;
+	printf("Print out a multiplication table");
+	printf("enter number of columns and rows");
+	fflush(stdout);
+	scanf("%d", &rows_Columns);
+	int multTable [rows_Columns] [rows_Columns];
+
+
+
+	//print out table
+	 int i,j,k = 0;
+	 	 //rows
+	     for(i = 0; i < rows_Columns ; i++)
+	     //columns
+	          for(j = 0; j < rows_Columns; j++)
+	        	  (j+1)*counter = multTable[i][j];
+	               multTable[i][j] = k++;
+
+	 float floatingPoint [];
 	return 0;
 }
 
